a4abf5946c771cf8c61959572e931da52eb8f2b1
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-04-13 22:59:27 -0700

    Use artist model to create new artist

--- middlewares/artists/controllers.js
+++ middlewares/artists/controllers.js
@@ -11,8 +11,25 @@ const artistsControllers = {
     });
   },
   addArtist: async (req, res) => {
+    const artist = await Artist.create({
+      slug: "aung-ko",
+      name: "Aung Ko",
+      photo:
+        "https://rivergallerymyanmar.com/public/storage/artists/November2019/Tu7zFZyISCjGsfMbB7YB.jpg",
+      biography: {
+        about:
+          "Born in Htone Bo, Myanmar in 1980.2002 Educated from University of Culture, Yangon.",
+        exhibitions: [
+          "2018 “Perupa Muda#3 Ring Road”, Sangkring Art Space, Yogyakarta",
+          "2018 “Imagine Beauty”, Sunrise Art Gallery & Arcade, Fairmont Hotel, Jakarta",
+          "2018 “Imagine Beauty”, Sunrise Art Gallery & Arcade, Fairmont Hotel, Jakarta",
+        ],
+      },
+    });
+
     res.status(200).send({
       message: "Add artist",
+      artist,
     });
   },
 };
